84d3971188322d2eb76d7d350f5fa2c7203a4e5d
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-12-09 00:39:33 -0300

    Variáveis ambiente com zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,12 @@
 import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
+import { env } from "@/env.ts";
 
 const server = fastify();
 
 server.register(fastifyCors, { origin: "*" });
+
+console.log(env);
 
 server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
